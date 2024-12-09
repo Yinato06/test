3111b68dc1bc258405032df6b59613ccb9da34a2
Yinato06 <alexander.akh06@gmail.com>
2024-12-09 22:02:37 +0300

    Update Task18.py

--- Task18.py
+++ Task18.py
@@ -3,8 +3,9 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
+    if first != 0:
         return first
+    return 1
 
 
 result = get_multiplied_digits(40203)
